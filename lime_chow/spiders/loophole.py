877ca0951595bf7d88a493f8af98ba8a3f7ccc1f
Emilio Almansi <ealmansi@gmail.com>
2023-09-19 00:38:54 +0200

    Add links to loophole

--- lime_chow/spiders/loophole.py
+++ lime_chow/spiders/loophole.py
@@ -1,4 +1,5 @@
 import scrapy
+import validators
 from lime_chow.items import EventItem
 from lime_chow.utils import EventUtils
 
@@ -8,30 +9,43 @@ class LoopholeSpider(scrapy.Spider):
     start_urls = ["http://loophole.berlin"]
 
     def parse(self, response):
-        for event in response.css(".post"):
-            venue = self.name
-            date = self.get_event_date(event)
-            title = event.css(
-                ".excerpt-title a::text"
-            ).extract_first().strip()
-            url = event.css(
-                ".excerpt-title a::attr(href)"
-            ).extract_first().strip()
-            thumbnail_url = event.css(
-                ".featured-image::attr(data-background)"
-            ).extract_first().strip()
-            yield EventItem(
-                id = EventUtils.build_id(venue, date, title),
-                extracted_at = EventUtils.get_current_datetime(),
-                venue = venue,
-                date = date,
-                title = title,
-                url = url,
-                thumbnail_url = thumbnail_url,
-                links = [],
-            )
+        for event_url in response.css(
+            ".excerpt-title a::attr(href)"
+        ).extract():
+            yield scrapy.Request(url=event_url, callback=self.parse_event)
+
+    def parse_event(self, response):
+        venue = self.name
+        date = self.get_event_date(response)
+        title = response.css(
+            ".entry-title::text"
+        ).extract_first().strip()
+        url = response.url
+        thumbnail_url = response.css(
+            ".featured-image::attr(style)"
+        ).extract_first().strip()
+        thumbnail_url = thumbnail_url[len("background-image: url("):-len(")")]
+        links = self.get_event_links(response)
+        yield EventItem(
+            id = EventUtils.build_id(venue, date, title),
+            extracted_at = EventUtils.get_current_datetime(),
+            venue = venue,
+            date = date,
+            title = title,
+            url = url,
+            thumbnail_url = thumbnail_url,
+            links = links,
+        )
 
     def get_event_date(self, event):
         date_long = event.css("span.date::text").extract_first()
         [day, month, year] = date_long.split("/")
         return "/".join([day, month, year[2:]])
+
+    def get_event_links(self, response):
+        links = response.css(
+            ".entry-content a::attr(href)"
+        ).extract()
+        links = list(filter(validators.url, links))
+        links = links[:10]
+        return links
