dd01e712f74d528461481ad6358f574c175880ba
Emilio Almansi <ealmansi@gmail.com>
2023-09-19 00:25:53 +0200

    Add links to peppi

--- lime_chow/spiders/peppi_guggenheim.py
+++ lime_chow/spiders/peppi_guggenheim.py
@@ -1,4 +1,5 @@
 import scrapy
+import validators
 from lime_chow.items import EventItem
 from lime_chow.utils import EventUtils
 
@@ -8,33 +9,45 @@ class PeppiGuggenheimSpider(scrapy.Spider):
     start_urls = ["https://www.peppi-guggenheim.de/?post_type=tribe_events"]
 
     def parse(self, response):
-        for event in response.css(".tribe-events-calendar-list__event-row"):
-            venue = self.name
-            date = self.get_event_date(event)
-            title = event.css(
-                ".tribe-events-calendar-list__event-title-link::text"
-            ).extract_first().strip()
-            url = event.css(
-                ".tribe-events-calendar-list__event-title-link::attr(href)"
-            ).extract_first().strip()
-            thumbnail_url = event.css(
-                ".tribe-events-calendar-list__event-featured-image::attr(src)"
-            ).extract_first().strip()
-            yield EventItem(
-                id = EventUtils.build_id(venue, date, title),
-                extracted_at = EventUtils.get_current_datetime(),
-                venue = venue,
-                date = date,
-                title = title,
-                url = url,
-                thumbnail_url = thumbnail_url,
-                links = [],
-            )
+        for event_url in response.css(
+            ".tribe-events-calendar-list__event-title-link::attr(href)"
+        ).extract():
+            yield scrapy.Request(url=event_url, callback=self.parse_event)
 
-    def get_event_date(self, event):
-        datetime = event.xpath("".join([
+    def parse_event(self, response):
+        venue = self.name
+        date = self.get_event_date(response)
+        title = response.css(
+            ".tribe-events-single-event-title::text"
+        ).extract_first().strip()
+        url = response.url
+        thumbnail_url = response.css(
+            ".tribe-events-event-image img::attr(src)"
+        ).extract_first().strip()
+        links = self.get_event_links(response)
+        yield EventItem(
+            id = EventUtils.build_id(venue, date, title),
+            extracted_at = EventUtils.get_current_datetime(),
+            venue = venue,
+            date = date,
+            title = title,
+            url = url,
+            thumbnail_url = thumbnail_url,
+            links = links,
+        )
+
+    def get_event_date(self, response):
+        abbr = response.xpath("".join([
             ".",
-            "//time",
-            "/@datetime",
+            "//abbr",
+            "/@title",
         ])).extract_first()
-        return "/".join(datetime[2:].split("-")[::-1])
+        return "/".join(abbr[2:].split("-")[::-1])
+
+    def get_event_links(self, response):
+        links = response.css(
+            ".tribe-events-single-event-description a::attr(href)"
+        ).extract()
+        links = list(filter(validators.url, links))
+        links = links[:10]
+        return links
