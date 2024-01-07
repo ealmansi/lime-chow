96015c76afa17a5ef875e474705aa84cbba3bed8
Emilio Almansi <ealmansi@gmail.com>
2024-01-07 17:00:57 +0100

    Update comedy cafe berlin spider

--- lime_chow/spiders/comedy_cafe_berlin.py
+++ lime_chow/spiders/comedy_cafe_berlin.py
@@ -15,25 +15,16 @@ class ComedyCafeBerlinSpider(scrapy.Spider):
         event_urls = response.css(
             ".tribe-events-pro-photo__event-title-link::attr(href)"
         ).extract()
-        self.logger.info("parse response.url %s", response.url)
-        self.logger.info("parse response.text %s", response.text)
-        self.logger.info("parse event_urls %s", ", ".join(event_urls))
         for event_url in event_urls:
             yield scrapy.Request(url=event_url, callback=self.parse_event)
 
     def parse_event(self, response):
-        self.logger.info("parse_event response.url %s", response.url)
-        self.logger.info("parse_event response.text %s", response.text)
         venue = self.name
         starts_on, starts_at = self.parse_starts_on(response)
-        title = (
-            response.css(".tribe-events-single-event-title::text")
-            .extract_first()
-            .strip()
-        )
+        title = response.css(".wp-block-heading::text").extract_first().strip()
         url = response.url
         thumbnail_url = (
-            response.css(".tribe-events-event-image img::attr(src)")
+            response.css(".wp-block-kadence-image img::attr(src)")
             .extract_first()
             .strip()
         )
@@ -56,8 +47,23 @@ class ComedyCafeBerlinSpider(scrapy.Spider):
         time = response.css(".tribe-events-schedule__time--start::text").extract_first()
         assert time is not None
         starts_at = date.strip() + " " + time.strip()
-        starts_at = datetime.strptime(starts_at, "%A, %B %d, %Y %I:%M%p")
+        starts_at = datetime.strptime(starts_at, "%A, %B %d %I:%M%p")
         starts_at = pytz.timezone("Europe/Berlin").localize(starts_at)
+        starts_at = self.guess_year(starts_at)
         starts_on = str(starts_at.date())
         starts_at = starts_at.isoformat()
         return starts_on, starts_at
+
+    def guess_year(self, starts_at):
+        now = pytz.timezone("Europe/Berlin").localize(datetime.now())
+        starts_at_prev = starts_at.replace(year=now.year - 1)
+        starts_at_curr = starts_at.replace(year=now.year)
+        starts_at_next = starts_at.replace(year=now.year + 1)
+        best = None
+        diff = None
+        for candidate in [starts_at_prev, starts_at_curr, starts_at_next]:
+            if best is None or abs(candidate - now) < diff:
+                best = candidate
+                diff = abs(candidate - now)
+        assert best is not None
+        return best
