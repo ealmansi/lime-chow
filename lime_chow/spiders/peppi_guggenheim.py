df34a9803211e8add07736a0a0db9c3c4a4a9016
Emilio Almansi <ealmansi@gmail.com>
2025-09-07 17:53:54 +0200

    fix peppi

--- lime_chow/spiders/peppi_guggenheim.py
+++ lime_chow/spiders/peppi_guggenheim.py
@@ -1,67 +1,45 @@
-import time
-from datetime import datetime
+from datetime import datetime, timedelta
+
+import pytz
+from icalevents import icalevents
 from lime_chow.items import EventItem
 from lime_chow.utils import EventUtils
-import pytz
 import scrapy
+import time
+from parsel import Selector
 import validators
 
 
 class PeppiGuggenheimSpider(scrapy.Spider):
     name = "peppi_guggenheim"
-    allowed_domains = ["peppi-guggenheim.de"]
-    start_urls = ["https://www.peppi-guggenheim.de/?post_type=tribe_events"]
+    allowed_domains = ["example.com"]
+    start_urls = ["https://www.example.com"]
 
-    def parse(self, response):
-        for event_url in response.css(
-            ".tribe-events-calendar-list__event-title-link::attr(href)"
-        ).extract():
-            yield scrapy.Request(url=event_url, callback=self.parse_event)
-
-    def parse_event(self, response):
-        venue = self.name
-        starts_on, starts_at = self.parse_starts_on(response)
-        title = (
-            response.css(".tribe-events-single-event-title::text")
-            .extract_first()
-            .strip()
-        )
-        url = response.url
-        thumbnail_url = (
-            response.css(".tribe-events-event-image img::attr(src)")
-            .extract_first()
-            .strip()
-        )
-        links = self.parse_links(response)
-        yield EventItem(
-            id=EventUtils.build_event_id(venue, starts_on, title),
-            venue=venue,
-            starts_on=starts_on,
-            starts_at=starts_at,
-            title=title,
-            url=url,
-            thumbnail_url=thumbnail_url,
-            links=links,
-            extracted_at=datetime.now().isoformat(),
-            ttl=int(time.time() + 36 * 3600),
-        )
-
-    def parse_starts_on(self, response):
-        date = response.css(".tribe-events-start-date::attr(title)").extract_first()
-        assert date is not None
-        time = response.css(".tribe-events-start-time::text").extract_first()
-        assert time is not None
-        starts_at = date.strip() + " " + time.strip()[: len("hh:mm")]
-        starts_at = datetime.strptime(starts_at, "%Y-%m-%d %H:%M")
-        starts_at = pytz.timezone("Europe/Berlin").localize(starts_at)
-        starts_on = str(starts_at.date())
-        starts_at = starts_at.isoformat()
-        return starts_on, starts_at
-
-    def parse_links(self, response):
-        links = response.css(
-            ".tribe-events-single-event-description a::attr(href)"
-        ).extract()
-        links = list(filter(validators.url, links))
-        links = links[:10]
-        return links
+    def parse(self, _):
+        url = "https://calendar.google.com/calendar/ical/jn2h4trf1n3u3ug4mh9vk1c0s4@group.calendar.google.com/public/basic.ics"
+        start_date = datetime.now()
+        end_date = datetime.now() + timedelta(days=30)
+        events = icalevents.events(url, start=start_date, end=end_date)
+        for event in events:
+            venue = self.name
+            starts_on = event.start.astimezone(pytz.timezone("Europe/Berlin"))
+            starts_at = starts_on.replace(
+                hour=20, minute=0, second=0, microsecond=0
+            ).isoformat()
+            starts_on = str(starts_on.date())
+            title = event.summary
+            links = Selector(text=event.description).css("a::attr(href)").getall()
+            links = list(filter(validators.url, links))
+            links = links[:10]
+            yield EventItem(
+                id=EventUtils.build_event_id(venue, starts_on, title),
+                venue=venue,
+                starts_on=starts_on,
+                starts_at=starts_at,
+                title=title,
+                url="https://peppi-guggenheim.de/",
+                thumbnail_url="https://peppi-guggenheim.de/file/i/6d6433a3440361412.jpg",
+                links=links,
+                extracted_at=datetime.now().isoformat(),
+                ttl=int(time.time() + 36 * 3600),
+            )
